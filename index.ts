b02d02fd482a99aa048e7861b0a3a1264124135a
Tobias-André <tobias.hole@gmail.com>
2025-01-28 18:56:38 +0100

    Added people and stuff

--- index.ts
+++ index.ts
@@ -3,6 +3,20 @@
  * @param firstname Tobias-André
  * @param lastname Hole
  */
+
 export const greeting = (firstname: string, lastname: string) => {
 	console.log(`Hello ${firstname} ${lastname}`);
 };
+
+const people = [
+	{ firstname: "Arne", lastname: "Arnesen" },
+	{ firstname: "Magnus", lastname: "Magnusson" },
+	{ firstname: "Siri", lastname: "Sirisen" },
+	{ firstname: "Stefan", lastname: "Stefanson" },
+  ];
+  
+  export const greetTheGang = () => {
+	people.forEach((person) => {
+	  greeting(person.firstname, person.lastname);
+	});
+  };
