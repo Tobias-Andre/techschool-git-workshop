59b7c81d4a88f1aa40d6483c6b543e45526e162b
Tobias-André <tobias.hole@gmail.com>
2025-01-28 19:20:49 +0100

    Yes

--- index.ts
+++ index.ts
@@ -7,16 +7,3 @@
 export const greeting = (firstname: string, lastname: string) => {
 	console.log(`Hello ${firstname} ${lastname}`);
 };
-
-const people = [
-	{ firstname: "Arne", lastname: "Arnesen" },
-	{ firstname: "Magnus", lastname: "Magnusson" },
-	{ firstname: "Siri", lastname: "Sirisen" },
-	{ firstname: "Stefan", lastname: "Stefanson" },
-  ];
-  
-  export const greetTheGang = () => {
-	people.forEach((person) => {
-	  greeting(person.firstname, person.lastname);
-	});
-  };
